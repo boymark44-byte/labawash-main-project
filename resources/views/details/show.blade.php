f9f83a2bfd0c7144b58cac44f30e4ea172946ca7
Dale Jann Paguntalan <dalejann.paguntalan@g.msuiit.edu.ph>
2022-07-14 10:19:53 +0800

    Create Show for Details

---
+++ resources/views/details/show.blade.php
@@ -0,0 +1,20 @@
+
+<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+   
+<div>
+    <h3>
+        {{$detail['name']}}
+    </h3>
+        <ul>
+            <li>
+                Type: {{$detail['type']}}
+            </li>
+            <li>
+                Location: {{$detail['location']}}
+            </li>
+            <li>
+                Year Established: {{$detail['year_created']}}
+            </li>
+        </ul> 
+</div>
+</div>
