ebfa412122d4c1772f04e444376c2a61d3eae0d7
Dan Marl Cadiz <danmarl.cadiz@g.msuiit.edu.ph>
2022-07-14 08:19:35 +0800

    Added roles to users and display on welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,6 +50,18 @@
                         <li><a href="/login">Login</a></li>
                         <li><a href="/register">Register</a></li>
                         @endauth
+                        @if ( Auth::user()->role == 1)
+                        <h1>SuperAdmin</h1>
+                        @endif
+                        @if ( Auth::user()->role == 2)
+                        <h1>Admin</h1>
+                        @endif
+                        @if ( Auth::user()->role == 3)
+                        <h1>Store</h1>
+                        @endif
+                        @if ( Auth::user()->role == 4)
+                        <h1>Costumer</h1>
+                        @endif
                     </ul>
                 </nav>
 
