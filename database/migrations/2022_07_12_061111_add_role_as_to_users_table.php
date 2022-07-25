390b92f9ee5d15ff911f76d0d7f403807a14276c
Dan Marl Cadiz <danmarl.cadiz@g.msuiit.edu.ph>
2022-07-25 00:58:57 +0000

    Minor change default user to customer

--- database/migrations/2022_07_12_061111_add_role_as_to_users_table.php
+++ database/migrations/2022_07_12_061111_add_role_as_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('role')->default('1');
+            $table->integer('role')->default('3');
         });
     }
 
