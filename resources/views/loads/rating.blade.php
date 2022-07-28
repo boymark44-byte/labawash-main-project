a9423cf84ea29b947550058583dff6363f233ac7
Elaine Miral <elainekrissnell.miral@g.msuiit.edu.ph>
2022-07-28 08:54:05 +0000

    added some changes

--- resources/views/loads/rating.blade.php
+++ resources/views/loads/rating.blade.php
@@ -3,20 +3,29 @@
 @section('content')
 
     <div class="container">
-        <p>Rate</p>
-        <div class="rate">
+        <div class="row justify-content-center">
+            <form action="">
+                <label>Send feedback?</label>
+                <input type="text">
+                <button class="btn">Submit</button>
+            </form>
 
-            <input type="radio" id="star5" name="rate" value="5" />
-            <label for="star5" title="text">5 stars</label>
-            <input type="radio" id="star4" name="rate" value="4" />
-            <label for="star4" title="text">4 stars</label>
-            <input type="radio" id="star3" name="rate" value="3" />
-            <label for="star3" title="text">3 stars</label>
-            <input type="radio" id="star2" name="rate" value="2" />
-            <label for="star2" title="text">2 stars</label>
-            <input type="radio" id="star1" name="rate" value="1" />
-            <label for="star1" title="text">1 star</label>
-
+            {{-- Charchar muna --}}
+            <p>Rate</p>
+            <div class="rate">
+                <input type="radio" id="star5" name="rate" value="5" />
+                <label for="star5" title="text">5 stars</label>
+                <input type="radio" id="star4" name="rate" value="4" />
+                <label for="star4" title="text">4 stars</label>
+                <input type="radio" id="star3" name="rate" value="3" />
+                <label for="star3" title="text">3 stars</label>
+                <input type="radio" id="star2" name="rate" value="2" />
+                <label for="star2" title="text">2 stars</label>
+                <input type="radio" id="star1" name="rate" value="1" />
+                <label for="star1" title="text">1 star</label>
+            </div>
+            <a href="{{ route('mycart') }}" class="btn">Submit</a>
         </div>
     </div>
+
 @endsection
